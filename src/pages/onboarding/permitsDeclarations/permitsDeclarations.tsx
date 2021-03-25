977855226a6d499c2e34860ffbc2cd201a720ee2
Michał Anioł <michal.aniol@solveq.io>
2021-03-25 17:20:03 +0100

    PermitsDeclarations by array, CheckBox as SVG

--- src/pages/onboarding/permitsDeclarations/permitsDeclarations.tsx
+++ src/pages/onboarding/permitsDeclarations/permitsDeclarations.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from "react";
 import { StyleSheet, Dimensions, View, Text, ScrollView, SafeAreaView } from 'react-native';
 import I18n from 'react-native-i18n';
 
+import deepCopy from '../../../helpers/deepCopy';
+
 import {
     setAppSize,
     setObjSize,
@@ -15,32 +17,54 @@ import {
 
 import StackHeader from '../../../sharedComponents/navi/stackHeader';
 import OnePermit from './onePermit';
-import BigRedBtn from '../../../sharedComponents/buttons/bigRedBtn'
+import BigRedBtn from '../../../sharedComponents/buttons/bigRedBtn';
 
 interface Props {
-    navigation: any
+    navigation: any,
 };
 
 const PermitsDeclarations: React.FC<Props> = (props: Props) => {
 
+    const permits = I18n.t('Permits');
+
+    interface staus { checked: boolean, wrong: boolean };
+    const permitStatus: Array<staus> = [];
+
+    permits.forEach(() => { permitStatus.push({ checked: false, wrong: false }) });
+    console.log('%c permitStatus:', 'background: #ffcc00; color: #003300', permitStatus)
+
+    const [status, setStatus] = useState(permitStatus);
     const [allPerm, setAllPerm] = useState(false);
     const [changeAll, setChangeAll] = useState(false);
-    const [perm_1, setPerm_1] = useState(false);
-    const [perm_2, setPerm_2] = useState(false);
-    const [perm_3, setPerm_3] = useState(false);
 
     useEffect(() => {
         if (!changeAll) return;
-        setPerm_1(allPerm);
-        setPerm_2(allPerm);
-        setPerm_3(allPerm);
+        let newStatus = deepCopy(status);
+        newStatus.forEach(e => e.checked = allPerm);
+        setStatus(newStatus);
     }, [allPerm])
 
-    useEffect(() => {
+    const handleChangeStatus = (num: number, val: boolean) => {
+        let newStatus = deepCopy(status);
+        newStatus[num].checked = val;
+        console.log('%c newStatus:', newStatus)
+
         setChangeAll(false);
-        if (perm_1 == false || perm_2 == false || perm_3 == false) setAllPerm(false)
-        if (perm_1 == true && perm_2 == true && perm_3 == true) setAllPerm(true)
-    }, [perm_1, perm_2, perm_3])
+        if (newStatus.some(e => !e.checked)) setAllPerm(false)
+        if (newStatus.every(e => e.checked)) setAllPerm(true)
+
+        setStatus(newStatus);
+    }
+
+    const hendlerGoFoward = () => {
+        // if (perm_1 && perm_2 && perm_3) {
+
+        // } else {
+        let newStatus = deepCopy(status);
+        newStatus.map(e => e.wrong = !e.checked);
+        setStatus(newStatus);
+        // }
+    }
 
     const ww = Dimensions.get('window').width;
     const wh = Dimensions.get('window').height;
@@ -70,55 +94,44 @@ const PermitsDeclarations: React.FC<Props> = (props: Props) => {
         }
     })
 
+
     return (
         <SafeAreaView>
             <View style={styles.scroll}>
                 <ScrollView>
 
-                    <Text style={[styles.text, { marginTop: getTop(50), fontSize: 30,color: '#313131' }]}>
+                    <Text style={[styles.text, { marginTop: getTop(50), fontSize: 30, color: '#313131' }]}>
                         {I18n.t('Permits-title')}
                     </Text>
 
-                    <Text style={[styles.text, { marginTop: getTop(6), fontSize: 18,color: '#555555' }]}>
+                    <Text style={[styles.text, { marginTop: getTop(6), fontSize: 18, color: '#555555' }]}>
                         {I18n.t('Permits-text')}
                     </Text>
 
                     <OnePermit
-                        check={allPerm}
+                        checked={allPerm}
                         getCheck={(val: boolean) => { setAllPerm(val); setChangeAll(true) }}
                         text='Zaznacz wszystkie'
                         marginTop={getTopPx(31)}
                         navigation={props.navigation}
                     ></OnePermit>
 
-                    <OnePermit
-                        check={perm_1}
-                        getCheck={setPerm_1}
-                        text={I18n.t('Permits-1')}
-                        marginTop={getTopPx(41)}
-                        navigation={props.navigation}
-                    ></OnePermit>
-
-                    <OnePermit
-                        check={perm_2}
-                        getCheck={setPerm_2}
-                        text={I18n.t('Permits-2')}
-                        marginTop={getTopPx(41)}
-                        navigation={props.navigation}
-                    ></OnePermit>
-
-                    <OnePermit
-                        check={perm_3}
-                        getCheck={setPerm_3}
-                        text={I18n.t('Permits-3')}
-                        marginTop={getTopPx(41)}
-                        navigation={props.navigation}
-                    ></OnePermit>
+                    {status.map((e: any, i: number) => (
+                        <OnePermit
+                            checked={e.checked}
+                            wrong={permits[i].required && e.wrong}
+                            getCheck={(val: boolean) => handleChangeStatus(i, val)}
+                            text={permits[i].text}
+                            marginTop={getTopPx(11)}
+                            navigation={props.navigation}
+                            key={'per_' + i}
+                        ></OnePermit>
+                    ))}
 
                     <View style={styles.btn}>
                         <BigRedBtn
                             title={I18n.t('Permits-btn')}
-                            onpress={() => { }}
+                            onpress={() => hendlerGoFoward()}
                         />
                     </View>
 
