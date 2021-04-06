49c13b80163ea0c691a6c97a10319f7ee19627cd
Michał Anioł <michal.aniol@solveq.io>
2021-04-06 14:14:51 +0200

    header stack bug position

--- src/sharedComponents/navi/stackHeader/topBackBtn.tsx
+++ src/sharedComponents/navi/stackHeader/topBackBtn.tsx
@@ -44,31 +44,28 @@ const TopBackBtn: React.FC<Props> = (props: Props) => {
             alignItems: 'center',
             justifyContent: 'center',
             // backgroundColor: '#d8232a',
-            padding: 8,
+            padding: 9,
             position: 'absolute',
             width: getWidthPxOf(40),
-            height: getHeightOfPx(34),
-            left: getLeftPx(30),
-            top: getTopPx(0)
+            height: getWidthPxOf(34),
+            left: getWidthPxOf(30),
+            top: getWidthPxOf(-2)
         },
     })
 
     return (
         <TouchableOpacity
             onPress={props.onpress}
+            style={styles.btn}
         >
-            <View
-                style={styles.btn}
-            >
-                <Svg viewBox="0 0 20 16">
-                    <Path
-                        fill="#313131"
-                        fillRule="nonzero"
-                        transform="translate(-40 -70) translate(0 1) translate(40 65) translate(0 2) translate(0 2.85)"
-                        d="M7.293 1.147c.36.342.388.881.083 1.254l-.083.09-3.879 3.684H19l.117.006c.497.055.883.457.883.944 0 .525-.448.95-1 .95H3.414l3.879 3.685.083.09c.305.372.277.91-.083 1.253-.39.371-1.024.371-1.414 0L.293 7.797l-.083-.09c-.305-.372-.278-.911.083-1.254l5.586-5.306c.39-.371 1.023-.371 1.414 0z"
-                    />
-                </Svg>
-            </View>
+            <Svg viewBox="0 0 20 16">
+                <Path
+                    fill="#313131"
+                    fillRule="nonzero"
+                    transform="translate(-40 -70) translate(0 1) translate(40 65) translate(0 2) translate(0 2.85)"
+                    d="M7.293 1.147c.36.342.388.881.083 1.254l-.083.09-3.879 3.684H19l.117.006c.497.055.883.457.883.944 0 .525-.448.95-1 .95H3.414l3.879 3.685.083.09c.305.372.277.91-.083 1.253-.39.371-1.024.371-1.414 0L.293 7.797l-.083-.09c-.305-.372-.278-.911.083-1.254l5.586-5.306c.39-.371 1.023-.371 1.414 0z"
+                />
+            </Svg>
         </TouchableOpacity>
     )
 }
