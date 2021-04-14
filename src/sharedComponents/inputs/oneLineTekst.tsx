b077d484a2cf9d694b0f00dd42c9f88a35322a3b
Michał Anioł <michal.aniol@solveq.io>
2021-04-14 22:22:40 +0200

    GetToKnowEachOther

--- src/sharedComponents/inputs/oneLineTekst.tsx
+++ src/sharedComponents/inputs/oneLineTekst.tsx
@@ -1,27 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, TextInput, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, TextInput, Text, View} from 'react-native';
 import I18n from 'react-native-i18n';
 
-import {
-    getHorizontalPx,
-    getVerticalPx
-} from '../../helpers/layoutFoo';
+import {getHorizontalPx, getVerticalPx} from '../../helpers/layoutFoo';
 
 interface Props {
     // * wartości wymagane
-    style?: any,
-    onChangeText: Function, // * zrtorka do rodzica wartości inputu
-    validationOk: Function, // funkcja validujaca popraność value w inpucie, zwraca true/false, zmienia border na zielono przy true
-    validationWrong: Function, // waliująca błędny value w inpucie (bo to nie zawsze jest owrotność warunku popraności), ture/false, zmienia border na czerwno przy true
-    messageWrong: string, // * informacja pod imputem jeśli validationWrong zwraca true
-    placeholder: string, // * npis na inputem
-    value: string, // * zmienia value w inpucie
-    validationStatus: Function, // zwrotka dla obiektu rodzica: poprawna gdy validationOk zwraca true, inaczej false
-    forceMessageWrong: string // informacja o błędnej validacji przez rodica, wg validationStatus, nadrzędna nad messageWrong
+    style?: any;
+    onChangeText: Function; // * zrtorka do rodzica wartości inputu
+    validationOk: Function; // funkcja validujaca popraność value w inpucie, zwraca true/false, zmienia border na zielono przy true
+    validationWrong: Function; // waliująca błędny value w inpucie (bo to nie zawsze jest owrotność warunku popraności), ture/false, zmienia border na czerwno przy true
+    messageWrong: string; // * informacja pod imputem jeśli validationWrong zwraca true
+    placeholder: string; // * npis na inputem
+    value: string; // * zmienia value w inpucie
+    validationStatus: Function; // zwrotka dla obiektu rodzica: poprawna gdy validationOk zwraca true, inaczej false
+    forceMessageWrong: string; // informacja o błędnej validacji przez rodica, wg validationStatus, nadrzędna nad messageWrong
 }
 
 const OneLineTekst: React.FC<Props> = (props: Props) => {
-
     const [borderColor, setBorderColor] = useState('#80555555');
     const [errorMessage, setErrorMessage] = useState('');
 
@@ -35,85 +31,88 @@ const OneLineTekst: React.FC<Props> = (props: Props) => {
 
         if (props.value == 'null' || props.value == 'NULL') {
             validation = 'bad';
-            message = I18n.t('OneLineTekst-error-null')
+            message = I18n.t('OneLineTekst-error-null');
         }
 
         if (props.validationWrong && props.validationWrong(props.value)) {
             validation = 'bad';
-            message = props.messageWrong ? props.messageWrong : ''
+            message = props.messageWrong ? props.messageWrong : '';
         }
 
         if (props.forceMessageWrong && props.forceMessageWrong != '') {
             validation = 'bad';
-            message = props.forceMessageWrong
+            message = props.forceMessageWrong;
         }
 
         switch (validation) {
-            case 'ok': {
-                setBorderColor('#2cba3f');
-                if (props.validationStatus) props.validationStatus(true)
-            }
+            case 'ok':
+                {
+                    setBorderColor('#2cba3f');
+                    if (props.validationStatus) {
+                        props.validationStatus(true);
+                    }
+                }
                 break;
-            case 'bad': {
-                setBorderColor('#d8232a');
-                if (props.validationStatus) props.validationStatus(false)
-            }
+            case 'bad':
+                {
+                    setBorderColor('#d8232a');
+                    if (props.validationStatus) {
+                        props.validationStatus(false);
+                    }
+                }
                 break;
             default:
                 {
                     setBorderColor('#80555555');
-                    if (props.validationStatus) props.validationStatus(false)
+                    if (props.validationStatus) {
+                        props.validationStatus(false);
+                    }
                 }
                 break;
         }
 
         setErrorMessage(message);
-
-    }, [props.value, props.forceMessageWrong])
-
+    }, [props.value, props.forceMessageWrong]);
 
     let styles = StyleSheet.create({
         light18: {
-            fontFamily: "DIN2014Narrow-Light",
-            fontSize: getHorizontalPx(18),
+            fontFamily: 'DIN2014Narrow-Light',
+            fontSize: 18,
             textAlign: 'left',
-            color: '#555555'
+            color: '#555555',
         },
         input: {
             display: 'flex',
             // alignItems: 'center',
             justifyContent: 'center',
             width: '100%',
-            borderRadius: getHorizontalPx(150),
-            fontFamily: "DIN2014Narrow-Regular",
-            fontSize: getHorizontalPx(20),
+            borderRadius: 25,
+            fontFamily: 'DIN2014Narrow-Regular',
+            fontSize: 20,
             textAlign: 'left',
             color: 'black',
-            borderWidth: getHorizontalPx(2),
+            borderWidth: 1,
             borderColor: borderColor,
-            height: getVerticalPx(50),
-            marginTop: getHorizontalPx(6),
-            paddingLeft: getHorizontalPx(30),
-            paddingTop: getHorizontalPx(10),
-            paddingBottom: getHorizontalPx(10)
+            height: 50,
+            marginTop: 6,
+            paddingLeft: 30,
+            paddingTop: 10,
+            paddingBottom: 10,
         },
         error: {
-            fontFamily: "DIN2014Narrow-Light",
-            fontSize: getHorizontalPx(18),
+            fontFamily: 'DIN2014Narrow-Light',
+            fontSize: 18,
             textAlign: 'left',
             color: '#d8232a',
             position: 'relative',
-            marginTop: (6),
-            height: getHorizontalPx(23)
-        }
-    })
+            marginTop: 6,
+            height: 23,
+        },
+    });
 
     return (
         <View style={props.style}>
-
-            <Text style={styles.light18}>
-                {props.placeholder}
-            </Text>
+            <Text style={styles.light18}>{props.placeholder}</Text>
 
             <TextInput
                 style={styles.input}
@@ -121,12 +120,9 @@ const OneLineTekst: React.FC<Props> = (props: Props) => {
                 value={props.value}
             />
 
-            <Text style={styles.error}>
-                {errorMessage}
-            </Text>
-
+            <Text style={styles.error}>{errorMessage}</Text>
         </View>
-    )
-}
+    );
+};
 
 export default OneLineTekst;
