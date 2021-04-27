495ccc12407a625b6be4c4f0f64dad589f3e8366
likrot <bramish@gmail.com>
2021-04-27 15:54:50 +0200

    operations description

--- src/pages/main/bike/rewiewsDetails/rewiewsDetails.tsx
+++ src/pages/main/bike/rewiewsDetails/rewiewsDetails.tsx
@@ -1,18 +1,22 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, ScrollView, SafeAreaView} from 'react-native';
+import {
+    StyleSheet,
+    View,
+    Text,
+    ScrollView,
+    SafeAreaView,
+} from 'react-native';
 import I18n from 'react-native-i18n';
 
 import {
     setObjSize,
     getVerticalPx,
-    getHeightPx,
     getHorizontalPx,
     getWidthPx,
     getCenterLeftPx,
 } from '../../../../helpers/layoutFoo';
 
 import StackHeader from '../../../../sharedComponents/navi/stackHeader/stackHeader';
-import BigRedBtn from '../../../../sharedComponents/buttons/bigRedBtn';
 import AnimSvg from '../../../../helpers/animSvg';
 import {countDaysToEnd} from '../../../../helpers/warranty';
 import ServiceMapBtn from '../../../../sharedComponents/buttons/serviceMap';
@@ -23,19 +27,11 @@ interface Props {
 }
 
 const RewiewsDetails: React.FC<Props> = (props: Props) => {
-    const trans = I18n.t('RewiewsDetails');
+    const trans: any = I18n.t('RewiewsDetails');
     const details = props.route.params.details;
     const box = props.route.params.box;
     const region = props.route.params.region;
     const location = props.route.params.location;
-    // console.log('%c details:', 'background: #ffcc00; color: #003300', details)
-
-    const placeIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
-    <path
-    fill="#d8232a" stroke="none"
-       d="M 17.500434,0 A 17.500033,17.500033 0 0 0 0,17.499565 17.500033,17.500033 0 0 0 17.500434,35 17.500033,17.500033 0 0 0 34.999998,17.499565 17.500033,17.500033 0 0 0 17.500434,0 Z m 0,7.284816 a 7.5885871,7.5885871 0 0 1 7.588423,7.589293 7.5885871,7.5885871 0 0 1 -1.508464,4.524519 l 0.01216,-8.91e-4 -0.08787,0.101782 a 7.5885871,7.5885871 0 0 1 -0.626352,0.720305 l -4.171325,4.797678 v 8.91e-4 l -1.206599,1.387543 -1.205726,-1.387543 -4.161757,-4.78637 a 7.5885871,7.5885871 0 0 1 -0.666369,-0.76641 l -0.05916,-0.06785 0.01131,8.91e-4 A 7.5885871,7.5885871 0 0 1 9.912004,14.874113 7.5885871,7.5885871 0 0 1 17.500426,7.28482 Z m 0,4.704596 a 3.2906114,3.2906114 0 0 0 -3.290954,3.290955 3.2906114,3.2906114 0 0 0 3.290954,3.290085 3.2906114,3.2906114 0 0 0 3.290086,-3.290085 3.2906114,3.2906114 0 0 0 -3.290086,-3.290955 z"
-        />
-  </svg>`;
 
     const [headHeight, setHeadHeightt] = useState(0);
 
@@ -88,6 +84,7 @@ const RewiewsDetails: React.FC<Props> = (props: Props) => {
             backgroundColor: '#d8232a',
             borderRadius: 30,
             marginTop: getVerticalPx(33),
+            marginBottom: getVerticalPx(123),
         },
         warningText: {
             fontFamily: 'DIN2014Narrow-Regular',
@@ -96,30 +93,19 @@ const RewiewsDetails: React.FC<Props> = (props: Props) => {
             lineHeight: 24,
             color: '#fff',
         },
+        optionsTextWrapper: {
+            marginBottom: 15,
+            marginTop: 30,
+            fontSize: 18,
+            fontFamily: 'DIN2014Narrow-Light',
+        },
+        optionsText: {
+            paddingVertical: 0.2,
+            letterSpacing: 0.5,
+            fontSize: 18,
+            fontFamily: 'DIN2014Narrow-Light',
+        },
     });
-
-    const hendleDay = (d: string) => {
-        const getDate = (dd: string) => {
-            let values = dd
-                .replace('Z', '')
-                .replace(/-|\s|T|:/g, '#')
-                .split('#')
-                .map(e => Number(e));
-            return new Date(...values);
-        };
-
-        let date = getDate(d);
-        return (
-            '' +
-            date.getDate() +
-            '.' +
-            date.getMonth() +
-            '.' +
-            date.getFullYear()
-        );
-    };
-
-    const heandlerGetWaranty = () => {};
 
     const heandleServicesMap = () => {
         if (!region || !location || !box) {
@@ -174,13 +160,10 @@ const RewiewsDetails: React.FC<Props> = (props: Props) => {
 
         setAnimSvgStyle({
             position: 'absolute',
-            // marginTop: getVerticalPx(10),
-
             left: -b,
             top: -b + getVerticalPx(40) - 2,
             width: w + b * 2,
             height: h + b * 2 + getVerticalPx(10),
-            // backgroundColor: 'grey'
         });
     };
 
@@ -188,12 +171,11 @@ const RewiewsDetails: React.FC<Props> = (props: Props) => {
         <SafeAreaView style={{backgroundColor: 'white'}}>
             <View style={styles.scroll}>
                 <ScrollView>
-
                     <View style={styles.area}>
-                    <AnimSvg
-                        source={source}
-                        style={[styles.animSvg, animSvgStyle]}
-                    />
+                        <AnimSvg
+                            source={source}
+                            style={[styles.animSvg, animSvgStyle]}
+                        />
                         <View
                             style={styles.titleBox}
                             onLayout={({nativeEvent}) =>
@@ -208,6 +190,24 @@ const RewiewsDetails: React.FC<Props> = (props: Props) => {
                                     trans.daysLeft[1]}
                             </Text>
                         </View>
+
+                        {details?.operations && (
+                            <View>
+                                <Text style={styles.optionsTextWrapper}>
+                                    {trans.descriptionTitle}
+                                </Text>
+                                {details.operations?.map((e: string) => {
+                                    return (
+                                        <Text
+                                            key={JSON.stringify(e)}
+                                            style={styles.optionsText}>
+                                            {' '}
+                                            - {e}
+                                        </Text>
+                                    );
+                                })}
+                            </View>
+                        )}
                     </View>
 
                     <ServiceMapBtn
