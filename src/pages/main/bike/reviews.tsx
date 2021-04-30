5827d534cddca8dd227aaeeaf26889574a9aa2ff
Michał Anioł <michal.janusz.aniol@gmail.com>
2021-04-30 16:11:35 +0200

    no  areas = [];

--- src/pages/main/bike/reviews.tsx
+++ src/pages/main/bike/reviews.tsx
@@ -58,7 +58,7 @@ const Reviews: React.FC<Props> = (props: Props) => {
 
     useEffect(() => {
         // setAreas([]);
-        areas = [];
+        // areas = [];
         startTicking();
     }, [props.list]);
 
