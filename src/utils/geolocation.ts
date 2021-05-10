13ea2284e11063bc94fae5efe8ec80cb992ab733
likrot <bramish@gmail.com>
2021-05-10 08:32:06 +0200

    changes

--- src/utils/geolocation.ts
+++ src/utils/geolocation.ts
@@ -1,9 +1,12 @@
 import BackgroundGeolocation from 'react-native-background-geolocation';
 
-export const initBGeolocalization = () => {
-    BackgroundGeolocation.ready({
+export const initBGeolocalization = async () => {
+    const state = await BackgroundGeolocation.ready({
         reset: true,
+        stopOnTerminate: false,
+        startOnBoot: true,
         desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
+        locationAuthorizationRequest: 'Always',
         locationAuthorizationAlert: {
             titleWhenNotEnabled: 'Location-services not enabled',
             titleWhenOff: 'Location-services OFF',
@@ -11,13 +14,19 @@ export const initBGeolocalization = () => {
             cancelButton: 'Cancel',
             settingsButton: 'Settings',
         },
+        backgroundPermissionRationale: {
+            title: "Allow access to this device's location in the background?",
+            message:
+                "In order to allow track activity when app is in the background, please enable 'Allow all the time permission",
+            positiveAction: 'Change to Allow all the time',
+        },
         distanceFilter: 40,
         stopTimeout: 5,
-        debug: false,
+        debug: __DEV__ ? true : false,
         logLevel: __DEV__
             ? BackgroundGeolocation.LOG_LEVEL_VERBOSE
-            : BackgroundGeolocation.LOG_LEVEL_ERROR,
-        stopOnTerminate: false,
-        startOnBoot: true,
+            : BackgroundGeolocation.LOG_LEVEL_OFF,
     });
+
+    return state;
 };
