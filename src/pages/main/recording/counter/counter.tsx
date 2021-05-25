bde158993bb8eab9f3df445c755128de85c5d9f5
Michał Anioł <michal.janusz.aniol@gmail.com>
2021-05-25 12:35:32 +0200

    onMessage console log

--- src/pages/main/recording/counter/counter.tsx
+++ src/pages/main/recording/counter/counter.tsx
@@ -226,7 +226,6 @@ const Counter: React.FC<Props> = ({navigation}: Props) => {
     }, [pageState]);
 
     const heandleOnMessage = e => {
-        console.log('onMessage:', e.nativeEvent.data);
         let val = e.nativeEvent.data.split(';');
 
         switch (val[0]) {
