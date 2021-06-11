d0e96e4e5acfdd2b1cc8106754eea7e1d385381a
Michał Anioł <michal.janusz.aniol@gmail.com>
2021-06-11 11:59:11 +0200

    feature/turtorial screens 4 & 5

---
+++ src/pages/onboarding/newBeginning/bike_5.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Svg, {G, Path, Circle, Ellipse} from 'react-native-svg';
+
+interface Props {
+    style: any;
+}
+
+const Bike_5: React.FC<Props> = (props: Props) => ();
+
+export default Bike_5;
