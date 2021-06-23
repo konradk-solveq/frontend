8a1bc776a9f2ac76df28b837ddeca52adce80a87
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2021-06-23 11:54:39 +0200

    handle before remove action

---
+++ src/hooks/useCustomSwipeBackNav.ts
@@ -0,0 +1,25 @@
+import {useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
+
+const useCustomSwipeBackNav = (
+    callback?: () => void,
+    abortActionDefault?: boolean,
+) => {
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const eventListener = navigation.addListener('beforeRemove', e => {
+            if (abortActionDefault) {
+                e.preventDefault();
+            }
+
+            if (callback) {
+                callback();
+            }
+        });
+
+        return eventListener;
+    }, [abortActionDefault, callback, navigation]);
+};
+
+export default useCustomSwipeBackNav;
