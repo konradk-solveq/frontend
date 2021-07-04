7e11bfc908260a4d23b59ec14b602cdcc9ea8dae
Sebatian Kasinski <sebastian.kasinski@solveq.io>
2021-07-04 21:01:27 +0200

    buttons are still responsive when usetouchableopacity from rn-gesture-handler

--- src/sharedComponents/buttons/bigWhiteBtn.tsx
+++ src/sharedComponents/buttons/bigWhiteBtn.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler'
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 import {getHorizontalPx} from '../../helpers/layoutFoo';
 
