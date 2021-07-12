92c881d0a3aaa85ff24269b40a2bb4eec4158d9c
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2021-07-12 17:51:27 +0200

    conditional persist key

--- src/storage/storage.tsx
+++ src/storage/storage.tsx
@@ -5,9 +5,28 @@ import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import reducer from './reducer';
 
-const buildStore = () => {
+const checkAppHasWrongPersistKey = async () => {
+    try {
+        const jsonValue = await AsyncStorage.getItem('persist:root_mykross');
+        if (jsonValue) {
+            return false;
+        }
+
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
+
+const buildStore = async () => {
+    let pKey = 'root_mykross';
+    const persistKeyIsWrong = await checkAppHasWrongPersistKey();
+    if (persistKeyIsWrong) {
+        pKey = 'root';
+    }
+
     const persistConfig = {
-        key: 'root_mykross',
+        key: pKey,
         storage: AsyncStorage,
         stateReconciler: autoMergeLevel2,
         timeout: 2000,
