94761557a585460e8b7d53dd6969d6d23b5be888
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2021-07-26 11:54:34 +0200

    check

--- src/hooks/useAuthorization.ts
+++ src/hooks/useAuthorization.ts
@@ -2,6 +2,7 @@ import {useEffect} from 'react';
 
 import {useAppDispatch, useAppSelector} from './redux';
 import {isOnlineAppStatusSelector} from '../storage/selectors';
+import {isGoodConnectionQualitySelector} from '../storage/selectors/app';
 import {checkSession, logIn, register} from '../storage/actions';
 import {
     authErrorSelector,
@@ -13,6 +14,9 @@ import {
 const useAuthorization = (active: boolean) => {
     const dispatch = useAppDispatch();
     const isOnline = useAppSelector(isOnlineAppStatusSelector);
+    const isGoodConnectionQuality = useAppSelector(
+        isGoodConnectionQualitySelector,
+    );
     const isRegistered = useAppSelector(isRegisteredSelector);
     const isAuthorized = useAppSelector(isAuthorizedSelector);
     const isAuthProcessing = useAppSelector(isLodingSelector);
@@ -20,24 +24,46 @@ const useAuthorization = (active: boolean) => {
 
     /* Registration */
     useEffect(() => {
-        if (active && isOnline && !isRegistered) {
+        if (active && isOnline && isGoodConnectionQuality && !isRegistered) {
             dispatch(register());
         }
-    }, [dispatch, active, isOnline, isRegistered]);
+    }, [dispatch, active, isOnline, isGoodConnectionQuality, isRegistered]);
 
     /* Log in */
     useEffect(() => {
-        if (isOnline && isRegistered && !isAuthorized) {
+        if (
+            isOnline &&
+            isGoodConnectionQuality &&
+            isRegistered &&
+            !isAuthorized
+        ) {
             dispatch(logIn());
         }
-    }, [dispatch, isOnline, isRegistered, isAuthorized]);
+    }, [
+        dispatch,
+        isOnline,
+        isGoodConnectionQuality,
+        isRegistered,
+        isAuthorized,
+    ]);
 
     /* Refresh session */
     useEffect(() => {
-        if (isOnline && isRegistered && isAuthorized) {
+        if (
+            isOnline &&
+            isGoodConnectionQuality &&
+            isRegistered &&
+            isAuthorized
+        ) {
             dispatch(checkSession());
         }
-    }, [dispatch, isOnline, isRegistered, isAuthorized]);
+    }, [
+        dispatch,
+        isOnline,
+        isGoodConnectionQuality,
+        isRegistered,
+        isAuthorized,
+    ]);
 
     return {
         authError,
