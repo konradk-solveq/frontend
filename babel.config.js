c662d203fb0ee7b810e0d1152d1a6171426e499c
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2021-08-03 15:12:55 +0200

    fix

--- babel.config.js
+++ babel.config.js
@@ -1,99 +1,18 @@
+const importPaths = require('./babelImportPaths');
+
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
         ['@babel/plugin-proposal-decorators', {legacy: true}],
-        [
-            'module:react-native-dotenv',
-            {
-                moduleName: '@env',
-            },
-        ],
+        ['module:react-native-dotenv', {moduleName: '@env'}],
         'react-native-reanimated/plugin',
-        [
-            'babel-plugin-root-import',
-            {
-                paths: [
-                    {
-                        rootPathSuffix: './src',
-                        rootPathPrefix: '@src',
-                    },
-                    {
-                        rootPathSuffix: './src/api',
-                        rootPathPrefix: '@api',
-                    },
-                    {
-                        rootPathSuffix: './src/assets',
-                        rootPathPrefix: '@assets',
-                    },
-                    {
-                        rootPathSuffix: './src/enums',
-                        rootPathPrefix: '@enums',
-                    },
-                    {
-                        rootPathSuffix: './src/helpers',
-                        rootPathPrefix: '@helpers',
-                    },
-                    {
-                        rootPathSuffix: './src/hooks',
-                        rootPathPrefix: '@hooks',
-                    },
-                    {
-                        rootPathSuffix: './src/interfaces',
-                        rootPathPrefix: '@interfaces',
-                    },
-                    {
-                        rootPathSuffix: './src/models',
-                        rootPathPrefix: '@models',
-                    },
-                    {
-                        rootPathSuffix: './src/navigation',
-                        rootPathPrefix: '@navigation',
-                    },
-                    {
-                        rootPathSuffix: './src/pages',
-                        rootPathPrefix: '@pages',
-                    },
-                    {
-                        rootPathSuffix: './src/services',
-                        rootPathPrefix: '@services',
-                    },
-                    {
-                        rootPathSuffix: './src/sharedComponents',
-                        rootPathPrefix: '@sharedComponents',
-                    },
-                    {
-                        rootPathSuffix: './src/storage',
-                        rootPathPrefix: '@storage',
-                    },
-                    {
-                        rootPathSuffix: './src/types',
-                        rootPathPrefix: '@types',
-                    },
-                    {
-                        rootPathSuffix: './src/utils',
-                        rootPathPrefix: '@utils',
-                    },
-                    {
-                        rootPathSuffix: './I18n',
-                        rootPathPrefix: '@translations',
-                    },
-                ],
-            },
-        ],
+        ['babel-plugin-root-import', {paths: importPaths}],
     ],
     env: {
         production: {
             plugins: [
                 'transform-remove-console',
-                'babel-plugin-root-import',
-                {
-                    paths: [
-                        {
-                            rootPathSuffix: './src',
-                            rootPathPrefix: '@',
-                        },
-                    ],
-                },
+                ['babel-plugin-root-import', {paths: importPaths}],
             ],
         },
     },
