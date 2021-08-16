5f8e1053406421ce856f6904167502bf0fa83af2
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2021-08-16 15:52:37 +0200

    expected results for synch

--- src/storage/actions/__tests__/utils/expectedAxtions.ts
+++ src/storage/actions/__tests__/utils/expectedAxtions.ts
@@ -1,5 +1,6 @@
 import * as actionTypes from '../../actionTypes';
-import {startedRoute, stoppedRoute} from './routeData';
+import {dataToSynch, endedRoute, startedRoute, stoppedRoute} from './routeData';
+import {I18n} from '@translations/I18n';
 
 export const startRecordingExpectedActions = [
     {
@@ -34,6 +35,88 @@ export const stopRecordingExpectedActions = [
     {
         isMapVisible: false,
         type: actionTypes.SET_ROUTE_MAP_VISIBILITY,
+    },
+    {
+        state: false,
+        type: actionTypes.SET_ROUTES_LOADING_STATE,
+    },
+];
+
+export const synchRecordingExpectedActions = [
+    {
+        state: true,
+        type: actionTypes.SET_ROUTES_LOADING_STATE,
+    },
+    {
+        type: actionTypes.SET_PRIVATE_MAPID_TO_ADD,
+        privateMapId: endedRoute.remoteRouteId,
+    },
+    {
+        type: actionTypes.CLEAR_CURRENT_ROUTE_DATA,
+        removeDuplicates: undefined,
+    },
+    {
+        type: actionTypes.CLEAR_CURRENT_ROUTE,
+        keepId: undefined,
+    },
+    {
+        type: actionTypes.SET_AVERAGE_ROUTE_SPEED,
+    },
+    {
+        type: actionTypes.CLEAR_ROUTES_ERROR,
+    },
+    {
+        state: false,
+        type: actionTypes.SET_ROUTES_LOADING_STATE,
+    },
+];
+
+export const synchRecordingWhenOfflineExpectedActions = [
+    {
+        state: true,
+        type: actionTypes.SET_ROUTES_LOADING_STATE,
+    },
+    {
+        state: true,
+        type: actionTypes.SET_ROUTES_LOADING_STATE,
+    },
+    {
+        type: actionTypes.SET_ROUTE_TO_SYNC,
+        routeId: endedRoute.id,
+    },
+    {
+        type: actionTypes.SET_ROUTES_DATA,
+        routes: {
+            id: endedRoute.id,
+            remoteRouteId: undefined,
+            route: dataToSynch,
+        },
+        refresh: undefined,
+    },
+    {
+        type: actionTypes.SET_AVERAGE_ROUTE_SPEED,
+    },
+    {
+        type: actionTypes.SET_ROUTES_ERROR,
+        error: I18n.t('dataAction.dataSyncError'), //read from translation
+        statusCode: 500,
+    },
+    {
+        state: false,
+        type: actionTypes.SET_ROUTES_LOADING_STATE,
+    },
+    {
+        type: actionTypes.CLEAR_CURRENT_ROUTE_DATA,
+        removeDuplicates: undefined,
+    },
+    {
+        type: actionTypes.CLEAR_CURRENT_ROUTE,
+        keepId: undefined,
+    },
+    {
+        type: actionTypes.SET_ROUTES_ERROR,
+        error: I18n.t('dataAction.noInternetConnection'), //read from translation
+        statusCode: 500,
     },
     {
         state: false,
