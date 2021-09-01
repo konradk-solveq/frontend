914f9505cb02f862538e78d00b74ff3e930687ba
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2021-09-02 01:59:19 +0200

    types

--- src/pages/main/recording/counter/nativeCounter/counterContext/counterContext.ts
+++ src/pages/main/recording/counter/nativeCounter/counterContext/counterContext.ts
@@ -1,13 +1,17 @@
 import React from 'react';
-import {DataI} from '../../../../../../hooks/useLocalizationTracker';
+
+import {ShortCoordsType} from '@type/coords';
+import {DataI} from '@hooks/useLocalizationTracker';
 
 type CounterDataType = {
     trackerData: DataI | undefined;
+    treackerDataAgregator: ShortCoordsType[];
     pauseTime: number;
 };
 
 export const CounterCallbackContext = React.createContext(() => {});
 export const CounterDataContext = React.createContext<CounterDataType>({
     trackerData: undefined,
+    treackerDataAgregator: [],
     pauseTime: 0,
 });
