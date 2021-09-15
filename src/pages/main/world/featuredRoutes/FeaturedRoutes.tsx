d300ca640d3ba59e2fa0c3cafc588dcb46ff466e
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2021-09-15 12:57:31 +0200

    featured horizontal list added

---
+++ src/pages/main/world/featuredRoutes/FeaturedRoutes.tsx
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/core';
+
+import {featuredMapsSelector} from '../../../../storage/selectors';
+import {useAppSelector} from '../../../../hooks/redux';
+import {Map} from '../../../../models/map.model';
+import {I18n} from '../../../../../I18n/I18n';
+
+import styles from './style';
+import ShowMoreModal from '../components/showMoreModal/showMoreModal';
+import {RegularStackRoute} from '../../../../navigation/route';
+import FeaturedRoutesList from './FeaturedRoutesList/FeaturedRoutesHorizontalList';
+
+interface RenderItem {
+    item: Map;
+    index: number;
+    sectionHeader?: string;
+}
+
+interface IProps {}
+
+const FeaturedRoutes: React.FC<IProps> = ({}: IProps) => {
+    const trans: any = I18n.t('MainWorld.PlannedRoutes');
+    const navigation = useNavigation();
+
+    const featuredMaps = useAppSelector(featuredMapsSelector);
+
+    const [showModal, setShowModal] = useState(false);
+    const [activeMapID, setActiveMapID] = useState<string>('');
+
+    const onPressHandler = (state: boolean, mapID?: string) => {
+        setShowModal(state);
+        if (mapID) {
+            setActiveMapID(mapID);
+        }
+    };
+
+    const onPressTileHandler = (mapID?: string) => {
+        navigation.navigate({
+            name: RegularStackRoute.ROUTE_DETAILS_SCREEN,
+            params: {
+                mapID: mapID,
+                private: false,
+                favourite: false,
+                featured: true,
+            },
+        });
+    };
+
+    const onShowMoreHandler = (sectionID: string) => {
+        console.log('[onShowMoreHandler -- pressed]');
+    };
+
+    if (!featuredMaps?.length) {
+        return null;
+    }
+
+    return (
+        <>
+            <ShowMoreModal
+                showModal={showModal}
+                mapID={activeMapID}
+                onPressCancel={() => onPressHandler(false)}
+                backdropStyle={styles.backdrop}
+                isFeatured
+            />
+            {featuredMaps.map(fm => {
+                const sectionHeader = fm?.section?.title;
+                console.log('[FM_SECTIon]', fm?.section);
+                return (
+                    <FeaturedRoutesList
+                        key={fm?.section?.id}
+                        data={fm}
+                        sectionID={fm?.section?.id}
+                        sectionHeader={sectionHeader}
+                        onPressMore={onPressHandler}
+                        onPressElement={onPressTileHandler}
+                        onShowMore={onShowMoreHandler}
+                    />
+                );
+            })}
+        </>
+    );
+};
+
+export default React.memo(FeaturedRoutes);
---
+++ src/pages/main/world/featuredRoutes/FeaturedRoutesList/FeaturedRoutesHorizontalList.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import {View, FlatList} from 'react-native';
+
+import {featuredMapsSelector} from '@storage/selectors';
+import {Map, FeaturedMapType} from '@models/map.model';
+import {useAppSelector} from '@hooks/redux';
+import {getVerticalPx} from '@helpers/layoutFoo';
+import {getImagesThumbs} from '@utils/transformData';
+import {I18n} from '@translations/I18n';
+
+import {TextBtn} from '@sharedComponents/buttons';
+
+import NextTile from '../../components/tiles/nextTile';
+
+import styles, {horizontalStyles} from './style';
+
+const getItemLayout = (_: any, index: number) => ({
+    length: getVerticalPx(175),
+    offset: getVerticalPx(175) * index,
+    index,
+});
+
+interface RenderItem {
+    item: Map;
+    index: number;
+}
+
+interface IProps {
+    data: FeaturedMapType;
+    sectionID?: string;
+    onPressMore: (state: boolean, mapID: string) => void;
+    onPressElement: (mapID?: string) => void;
+    onShowMore: (sectionID: string) => void;
+    sectionHeader?: string;
+}
+
+const FeaturedRoutesHorizontalList: React.FC<IProps> = ({
+    data,
+    sectionID,
+    onPressMore,
+    onPressElement,
+    onShowMore,
+    sectionHeader,
+}: IProps) => {
+    const trans: any = I18n.t('MainWorld.FeaturedRoutes');
+    const featuredMaps = useAppSelector(featuredMapsSelector);
+
+    const onPressHandler = (state: boolean, mapID?: string) => {
+        if (onPressMore && mapID) {
+            onPressMore(state, mapID);
+        }
+    };
+
+    const onShowMoreHandler = () => {
+        if (sectionID) {
+            onShowMore(sectionID);
+        }
+    };
+
+    const renderItem = ({item, index}: RenderItem) => {
+        if (index > 2) {
+            return null;
+        }
+
+        const middleTiles =
+            index !== 2 ? horizontalStyles.middleTile : undefined;
+        const images = getImagesThumbs(item?.images || []);
+
+        return (
+            <View key={item.id} style={[styles.tileWrapper, middleTiles]}>
+                <NextTile
+                    mapData={item}
+                    images={images}
+                    onPress={onPressHandler}
+                    onPressTile={onPressElement}
+                    tilePressable
+                    headerTitle={sectionHeader}
+                />
+            </View>
+        );
+    };
+
+    if (!featuredMaps?.length) {
+        return null;
+    }
+
+    return (
+        <>
+            <View>
+                <FlatList
+                    keyExtractor={item => item.id}
+                    data={data?.routes?.elements}
+                    renderItem={renderItem}
+                    showsVerticalScrollIndicator={false}
+                    getItemLayout={getItemLayout}
+                    initialNumToRender={3}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                />
+                <TextBtn text={trans.showMoreBtn} onPress={onShowMoreHandler} />
+            </View>
+        </>
+    );
+};
+
+export default React.memo(FeaturedRoutesHorizontalList);
