d1540acdc90a178712bdbbea45d5820d1dc9d444
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2021-10-07 14:19:26 +0200

    dispatch type

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -1,6 +1,10 @@
+import {AnyAction} from 'redux';
 import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
-import type {RootState, AppDispatch} from '../storage/storage';
+import {ThunkDispatch} from 'redux-thunk';
+
+import type {RootState} from '@storage/storage';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
+export type AppDispatch = ThunkDispatch<RootState, any, AnyAction>;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
