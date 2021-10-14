15c048952d1e9036b3dca6b09a49ca5d9775d328
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2021-10-15 01:52:53 +0200

    package rn-dotenv removed to avoid conflicts with rn-config package

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,6 @@ module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
         ['@babel/plugin-proposal-decorators', {legacy: true}],
-        ['module:react-native-dotenv', {moduleName: '@env'}],
         'react-native-reanimated/plugin',
         ['babel-plugin-root-import', {paths: importPaths}],
     ],
---
+++ src/env/index.ts
@@ -0,0 +1,7 @@
+import Config from 'react-native-config';
+
+export const API_URL = Config.API_URL;
+export const TESTING_MODE = Config.TESTING_MODE;
+export const FLIPPER_REDUX_DEBUGGER = Config.FLIPPER_REDUX_DEBUGGER;
+export const SENTRY_DSN = Config.SENTRY_DSN;
+export const ENVIRONMENT_TYPE = Config.ENVIRONMENT_TYPE;
