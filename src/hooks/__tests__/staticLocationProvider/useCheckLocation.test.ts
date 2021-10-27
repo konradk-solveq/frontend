e38bf0db05102c2b7dd074cfe87682b8c7075b54
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2021-10-27 20:38:23 +0200

    cleanup

--- src/hooks/__tests__/staticLocationProvider/useCheckLocation.test.ts
+++ src/hooks/__tests__/staticLocationProvider/useCheckLocation.test.ts
@@ -42,7 +42,6 @@ describe('[useCheckLocation]', () => {
             await waitForNextUpdate();
         });
 
-        console.log('location', result.current.locationType);
         expect(result.current.locationType).toBe('ALWAYS');
     }, 15000);
 
