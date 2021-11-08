40c70c5dac5fd5f81dd65ddb27579cdbf59000e6
Michal Aniol <michal.aniol@solveq.io>
2021-11-08 14:11:55 +0100

    rebase

--- src/sharedComponents/svg/like.tsx
+++ src/sharedComponents/svg/like.tsx
@@ -41,7 +41,7 @@ const Like: React.FC<IProps> = ({
         text: {
             fontFamily: 'DIN2014Narrow-Regular',
             color: '#313131',
-            fontSize: getFontSize(23),
+            fontSize: getFontSize(20),
             position: 'relative',
             marginLeft: getHorizontalPx(5),
         },
