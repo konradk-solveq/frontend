c0d73a65e3520de225fd0b4820dde2ffe256a8de
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2021-11-19 15:16:29 +0100

    added additional flavour for android => feat

--- src/types/react-native-config.d.ts
+++ src/types/react-native-config.d.ts
@@ -3,6 +3,10 @@ declare module 'react-native-config' {
     export const TESTING_MODE: 'true' | 'false';
     export const FLIPPER_REDUX_DEBUGGER: 'true' | 'false';
     export const SENTRY_DSN: string;
-    export const ENVIRONMENT_TYPE: 'production' | 'development' | 'test';
+    export const ENVIRONMENT_TYPE:
+        | 'production'
+        | 'development'
+        | 'test'
+        | 'feature';
     export const ROUTE_DEBUG_MODE: 'true' | 'false';
 }
