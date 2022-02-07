4e710e18c3693f875f54e4dfaebaf1c8fc053435
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2022-02-07 08:16:30 +0100

    version up

--- index.js
+++ index.js
@@ -4,10 +4,12 @@
 
 import 'react-native-gesture-handler';
 import 'react-native-get-random-values';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {initSentry} from '@sentryLogger/initSentry';
+
+LogBox.ignoreLogs(['new NativeEventEmitter']);
 
 if (!__DEV__ && !process.env.JEST_WORKER_ID) {
     initSentry();
