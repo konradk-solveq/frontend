9c7eb4f5c5995c0222e09042021b85100533b2f0
Sebatian Kasinski <sebastian.kasinski@solveq.io>
2022-04-12 18:12:04 +0200

    bugfix(MKA-333): fix for multiple modals on IOS

--- src/pages/main/world/components/modals/MoreActionsModal.tsx
+++ src/pages/main/world/components/modals/MoreActionsModal.tsx
@@ -1,8 +1,23 @@
+/**
+ * There is an issue on IOS (this is RN issue). We cannot show
+ * multiple modals at the same time.
+ *
+ * Workaround is nesting them or wait until first
+ * has been dismissed.
+ *
+ * In this component we wait until first modal (bottom action list)
+ * is hidden (component is animated).
+ *
+ * Future possible [solution](https://github.com/software-mansion/react-native-screens/pull/1096)
+ *  which doesn't work right now.
+ */
+
 import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import {Modal, StyleSheet, View} from 'react-native';
 
 import {MapsTypesT, RouteDetailsActionT} from '@type/screens/routesMap';
 import {useMergedTranslation} from '@utils/translations/useMergedTranslation';
+import {isIOS} from '@utils/platform';
 
 import {Backdrop} from '@components/backdrop';
 import {TransparentButton} from '@components/buttons';
@@ -13,6 +28,9 @@ import {Alert} from '@components/alerts';
 import {appContainerHorizontalMargin} from '@theme/commonStyle';
 import {getFVerticalPx} from '@theme/utils/appLayoutDimensions';
 import colors from '@theme/colors';
+
+const TIME_TO_HIDE_MODAL = isIOS ? 400 : 800;
+const TIME_TO_SHOW_SECOND_MODAL = isIOS ? TIME_TO_HIDE_MODAL + 50 : 0;
 
 interface IProps {
     onPressAction: (actionType: RouteDetailsActionT) => void;
@@ -51,7 +69,7 @@ const MoreActionsModal: React.FC<IProps> = ({
             () => {
                 setVisible(show);
             },
-            show ? 0 : 800,
+            show ? 0 : TIME_TO_HIDE_MODAL,
         );
 
         return () => clearTimeout(timer);
@@ -64,7 +82,9 @@ const MoreActionsModal: React.FC<IProps> = ({
 
     const showAdditionalAprove = useCallback(() => {
         onClose();
-        setAproveAction(true);
+        setTimeout(() => {
+            setAproveAction(true);
+        }, TIME_TO_SHOW_SECOND_MODAL);
     }, [onClose]);
 
     return (
