0924e91b8b71f32b5952deb1dbbde890e3246aba
Sebatian Kasinski <sebastian.kasinski@solveq.io>
2022-04-12 17:26:36 +0200

    cleanup diff tags

--- storybook/stories/index.ts
+++ storybook/stories/index.ts
@@ -4,23 +4,17 @@ import '@components/backdrop/__stories__';
 import '@components/buttons/__stories__';
 import '@components/divider/__stories__';
 import '@components/dropdown/__stories__';
+import '@components/headers/__stories__';
 import '@components/icons/__stories__';
 import '@components/inputs/__stories__';
 import '@components/modals/__stories__';
+import '@components/pagination/__stories__';
 import '@components/placeholders/__stories__';
-<<<<<<< HEAD
 import '@components/slider/__stories__';
 import '@components/tiles/__stories__';
-=======
-import '@components/headers/__stories__';
-import '@components/pagination/__stories__';
->>>>>>> 4c20182d8b57a30302237e7641bed5294065664c
 
 import '@containers/AddBike/__stories__';
 import '@containers/Bike/__stories__';
-<<<<<<< HEAD
 import '@containers/World/__stories__';
 import '@containers/World/components/__stories__';
-=======
 import '@containers/Onboarding/__stories__';
->>>>>>> 4c20182d8b57a30302237e7641bed5294065664c
