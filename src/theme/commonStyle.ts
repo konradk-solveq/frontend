e6bb59df1b9a7034284e7b3dcc2520da7f111483
Sebastian Kasiński <sebastian.kasinski@solveq.io>
2022-05-05 06:56:47 +0000

    Merge branch 'bugfix/fix-for-wrong-layout-utils-used-for-global-styles' into 'develop'
    bugifx: wrong function replaced with proper ones, snapshots updated
    
    bugifx: wrong function replaced with proper ones, snapshots updated
    
    bugifx: wrong function replaced with proper ones, snapshots updated

--- src/theme/commonStyle.ts
+++ src/theme/commonStyle.ts
@@ -1,14 +1,17 @@
 import {Dimensions, StyleSheet} from 'react-native';
 
 import {getAppLayoutConfig as get} from '@theme/appLayoutConfig';
-import {getVerticalPx, getHorizontalPx} from '@helpers/layoutFoo';
 import {isIOS} from '@utils/platform';
+import {
+    getFHorizontalPx,
+    getFVerticalPx,
+} from '@theme/utils/appLayoutDimensions';
 
 const {width, height} = Dimensions.get('screen');
 
-export const navBarHeight = getVerticalPx(100);
-export const appContainerHorizontalMargin = getHorizontalPx(16);
-export const appBottomMargin = getVerticalPx(40);
+export const navBarHeight = getFVerticalPx(88);
+export const appContainerHorizontalMargin = getFHorizontalPx(16);
+export const appBottomMargin = getFVerticalPx(40);
 export const screenWidth = width;
 export const screenHeight = height;
 export const SMALL_SCREEN_HEIGHT = 670;
@@ -22,7 +25,7 @@ export const commonStyle = StyleSheet.create({
     scroll: {
         position: 'absolute',
         width: '100%',
-        height: getVerticalPx(796) + (isIOS ? 0 : get.statusBarH()),
+        height: getFVerticalPx(796) + (isIOS ? 0 : get.statusBarH()),
         top: navBarHeight,
     },
 });
