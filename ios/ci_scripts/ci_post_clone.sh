9d3fbdfa97ae1452e5e36d71ca9336d2c1fc3c18
Sebatian Kasinski <sebastian.kasinski@solveq.io>
2022-09-01 10:32:12 +0200

    feature(MKA-681): Deploy screen for IOS

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  myKROSS
+#
+#  Created by Sebastian Kasiński on 31/08/2022.
+#
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+brew install cocoapods
+# have to add node yourself
+brew install node@14
+# link it to the path
+brew link node@14
+
+brew install npm
+
+# Install dependencies you manage with CocoaPods.
+npm install
+pod install
